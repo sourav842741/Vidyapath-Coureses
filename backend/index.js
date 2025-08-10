d136a1b5b3c12da82c928ae81b19a845ef53b694
Sourav kumar <161188554+sourav842741@users.noreply.github.com>
2025-08-10 23:06:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://vidyapath-coureses-1.onrender.com",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
